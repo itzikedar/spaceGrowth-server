f31889e8382df9a2cf5ce37aaa9541da27b1965d
Nitzan Lev <nitzanl@breezometer.com>
2020-11-08 19:03:32 +0200

    mockData

---
+++ content/mockData.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    userId: 1,
+    department: 3,
+    yearsInOrg: "4-6",
+    employeesManaged: "1-3",
+    phoneNumber: "0502243456",
+  },
+  {
+    userId: 2,
+    department: 1,
+    yearsInOrg: "4-6",
+    employeesManaged: "4-10",
+    phoneNumber: "0505463456",
+  },
+  {
+    userId: 3,
+    department: 5,
+    yearsInOrg: "4-6",
+    employeesManaged: "11-30",
+    phoneNumber: "0502262456",
+  },
+  {
+    userId: 4,
+    department: 6,
+    yearsInOrg: "4-6",
+    employeesManaged: "30-100",
+    phoneNumber: "0502243257",
+  },
+  {
+    userId: 5,
+    department: 6,
+    yearsInOrg: "4-6",
+    employeesManaged: "30-100",
+    phoneNumber: "0502243257",
+  },
+];
